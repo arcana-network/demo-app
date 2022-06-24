03994da2c59c32c2955f3b8deaeee6ccee34ccf6
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-06-24 13:55:23 +0530

    Change wallet.service to auth.service and change method names

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,20 +1,16 @@
+import { AuthProvider, computeAddress } from "@arcana/auth";
+
 const ARCANA_APP_ID = import.meta.env.VITE_ARCANA_APP_ID;
-const ARCANA_AUTH_NETWORK = import.meta.env.VITE_ARCANA_AUTH_NETWORK;
-const ARCANA_WALLET_URL = import.meta.env.VITE_ARCANA_WALLET_URL;
 const ARCANA_WALLET_APP_MODE = import.meta.env.VITE_ARCANA_WALLET_APP_MODE;
 
-const { AppMode, WalletProvider, computeAddress } = window.arcana.wallet;
-
-function createWalletService() {
-  const wallet = new WalletProvider({
-    appId: ARCANA_APP_ID,
-    iframeUrl: ARCANA_WALLET_URL,
-    network: ARCANA_AUTH_NETWORK,
-    inpageProvider: true,
-  });
+function createAuthService() {
+  const wallet = new AuthProvider(ARCANA_APP_ID);
 
   async function init() {
-    await wallet.init({ appMode: Number(ARCANA_WALLET_APP_MODE) });
+    await wallet.init({
+      appMode: Number(ARCANA_WALLET_APP_MODE),
+      position: "right",
+    });
   }
 
   async function isLoggedIn() {
@@ -26,24 +22,24 @@ function createWalletService() {
   }
 
   async function requestPublicKey(email) {
-    return await wallet.requestPublicKey(email);
+    return await wallet.getPublicKey(email);
   }
 
   async function requestSocialLogin(type) {
-    await wallet.requestSocialLogin(type);
+    await wallet.loginWithSocial(type);
   }
 
   async function requestUserInfo() {
-    return await wallet.requestUserInfo();
+    return await wallet.getUser();
   }
 
   async function requestWalletInfo() {
-    const provider = wallet.getProvider();
+    const provider = wallet.provider;
     return await provider.request({ method: "eth_accounts" });
   }
 
   function setHook(event, handler) {
-    const provider = wallet.getProvider();
+    const provider = wallet.provider;
     provider.on(event, handler);
   }
 
@@ -60,6 +56,6 @@ function createWalletService() {
   };
 }
 
-const WalletService = Object.freeze(createWalletService());
+const AuthService = Object.freeze(createAuthService());
 
-export default WalletService;
+export default AuthService;
