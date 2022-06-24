26e50cc4079bbae3a724d7f4554ee2b45abe9009
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-06-24 15:37:29 +0530

    Install polyfill dependencies
    - Add @rollup/plugin-inject
    - Add node-stdlib-browser

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,39 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
+import stdLibBrowser from "node-stdlib-browser";
+import inject from "@rollup/plugin-inject";
+
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      ...stdLibBrowser,
     },
   },
-  plugins: [vue()],
+  optimizeDeps: {
+    include: ["buffer", "process"],
+  },
+  plugins: [
+    vue(),
+    {
+      ...inject({
+        global: [
+          require.resolve("node-stdlib-browser/helpers/esbuild/shim"),
+          "global",
+        ],
+        process: [
+          require.resolve("node-stdlib-browser/helpers/esbuild/shim"),
+          "process",
+        ],
+        Buffer: [
+          require.resolve("node-stdlib-browser/helpers/esbuild/shim"),
+          "Buffer",
+        ],
+      }),
+      enforce: "post",
+    },
+  ],
   build: {
     chunkSizeWarningLimit: 1024,
     sourcemap: true,
