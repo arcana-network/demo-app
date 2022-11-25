1a6f33794d33f7b6a110c191e40dff33506c8d05
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-11-25 21:58:25 +0530

    Plug and play auth (#82)
    * Update sdks
    
    -  Storage SDK to `0.3.16`
    - Auth SDK to `0.2.0-beta.2`
    
    * Remove wallet mode
    
    * Update auth sdk
    
    * Update env to use auth config from env
    
    * Use plug and play auth for demo app
    
    * Rename connect wallet to auth popup
    
    * Rename onConnect to onLogin
    
    * Update auth sdk to 0.2.0-beta.1
    
    * Remove unused styles

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,19 +2,25 @@ import { AuthProvider } from "@arcana/auth";
 
 const ARCANA_APP_ADDRESS = import.meta.env.VITE_ARCANA_APP_ADDRESS;
 const ARCANA_AUTH_NETWORK = import.meta.env.VITE_ARCANA_AUTH_NETWORK;
-const ARCANA_WALLET_APP_MODE = import.meta.env.VITE_ARCANA_WALLET_APP_MODE;
+const ARCANA_HAS_WIDGET_MODE = import.meta.env.VITE_ARCANA_HAS_WIDGET_MODE;
+const ARCANA_WALLET_POSITION = import.meta.env.VITE_ARCANA_WALLET_POSITION;
+const ARCANA_WALLET_THEME = import.meta.env.VITE_ARCANA_WALLET_THEME;
 
 function createAuthService() {
   const auth = new AuthProvider(ARCANA_APP_ADDRESS, {
     network: ARCANA_AUTH_NETWORK,
     debug: true,
+    alwaysVisible: ARCANA_HAS_WIDGET_MODE === "true" ? false : true,
+    position: ARCANA_WALLET_POSITION,
+    theme: ARCANA_WALLET_THEME,
   });
 
   async function init() {
-    await auth.init({
-      appMode: Number(ARCANA_WALLET_APP_MODE),
-      position: "right",
-    });
+    await auth.init();
+  }
+
+  async function connect() {
+    await auth.connect();
   }
 
   async function isLoggedIn() {
@@ -60,6 +66,7 @@ function createAuthService() {
     requestUserInfo,
     requestWalletInfo,
     setHook,
+    connect,
   };
 }
 
